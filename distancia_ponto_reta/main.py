004e38ea66f3b3e025943123b355349aee5933cf
DanielNasc <danielnasc15987@gmail.com>
2022-01-08 09:43:33 -0300

    init

---
+++ distancia_ponto_reta/main.py
@@ -0,0 +1,16 @@
+from sys import path, exit
+path.insert(1, '../')
+from vectors import *
+
+"""
+    A distância de um ponto P(x0, y0) a uma reta ax + by + c = 0 é dada por:
+    d = |ax0 + by0 + c| / sqrt(a^2 + b^2)
+    e a distância de um ponto P(x0, y0, z0) a uma reta ax + by + cz + d = 0 é dada por:
+    d = |ax0 + by0 + cz0 + d| / sqrt(a^2 + b^2 + c^2)
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
